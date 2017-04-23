e072da9c966d366b331f2a5aed7f46c3e6402c21
Tijmen Wierenga <t.wierenga@live.nl>
2017-04-23 16:26:00 +0200

    Added container to app

--- src/Timesheets/Infrastructure/App.php
+++ src/Timesheets/Infrastructure/App.php
@@ -1,10 +1,27 @@
 <?php
 namespace TijmenWierenga\Project\Timesheets\Infrastructure;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * @author Tijmen Wierenga <t.wierenga@live.nl>
  */
 class App 
 {
-    
+    /**
+     * @var ContainerInterface
+     */
+    private static $container;
+
+    public function run(): void
+    {
+        self::$container = new ContainerBuilder();
+        self::$container->compile();
+    }
+
+    public static function getContainer(): ContainerInterface
+    {
+        return self::$container;
+    }
 }
