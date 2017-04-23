d98e361a4ee1483c3eaa52aceb7ab30c59186e77
Tijmen Wierenga <t.wierenga@live.nl>
2017-04-23 16:02:57 +0200

    Added test for WorkLogwasCreated domain event

---
+++ tests/Timesheets/Domain/Model/WorkLog/WorkLogWasCreatedTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace TijmenWierenga\Project\Tests\Timesheets\Domain\Model\WorkLog;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use TijmenWierenga\Project\Timesheets\Domain\Model\User\UserId;
+use TijmenWierenga\Project\Timesheets\Domain\Model\ValueObject\TimeFrame;
+use TijmenWierenga\Project\Timesheets\Domain\Model\WorkLog\WorkLogId;
+use TijmenWierenga\Project\Timesheets\Domain\Model\WorkLog\WorkLogWasCreated;
+
+/**
+ * @author Tijmen Wierenga <t.wierenga@live.nl>
+ */
+class WorkLogWasCreatedTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function a_work_log_was_created_event_has_a_timestamp()
+    {
+    	$event = new WorkLogWasCreated(WorkLogId::new(), UserId::new(), TimeFrame::new(
+            new DateTimeImmutable("2017-03-31T08:00:00"),
+            new DateTimeImmutable("2017-03-31T17:00:00")
+        ));
+
+    	$this->assertInstanceOf(DateTimeImmutable::class, $event->occurredOn());
+    }
+}
