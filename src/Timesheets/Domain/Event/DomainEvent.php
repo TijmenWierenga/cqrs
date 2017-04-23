7ef2410cb7aa6978ad3d9fd2a0176ddd66cf5ec5
Tijmen Wierenga <t.wierenga@live.nl>
2017-04-23 15:59:47 +0200

    Added OccurredOn interface method

--- src/Timesheets/Domain/Event/DomainEvent.php
+++ src/Timesheets/Domain/Event/DomainEvent.php
@@ -2,10 +2,17 @@
 
 namespace TijmenWierenga\Project\Timesheets\Domain\Event;
 
+use DateTimeImmutable;
+
 /**
  * @author Tijmen Wierenga <tijmen.wierenga@devmob.com>
  */
 interface DomainEvent
 {
-
+    /**
+     * Returns the date and time when the event occurred.
+     *
+     * @return DateTimeImmutable
+     */
+    public function occurredOn(): DateTimeImmutable;
 }
