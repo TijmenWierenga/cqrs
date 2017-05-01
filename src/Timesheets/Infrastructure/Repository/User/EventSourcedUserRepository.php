f202eec1ee37b1b7f01288590b546892a4c15f42
Tijmen Wierenga <t.wierenga@live.nl>
2017-05-01 22:20:05 +0200

    Added Projector to UserRepository

--- src/Timesheets/Infrastructure/Repository/User/EventSourcedUserRepository.php
+++ src/Timesheets/Infrastructure/Repository/User/EventSourcedUserRepository.php
@@ -2,10 +2,12 @@
 namespace TijmenWierenga\Project\Timesheets\Infrastructure\Repository\User;
 
 use TijmenWierenga\Project\Timesheets\Domain\Event\EventStore;
+use TijmenWierenga\Project\Timesheets\Domain\Event\EventStream;
 use TijmenWierenga\Project\Timesheets\Domain\Exception\ModelNotFoundException;
 use TijmenWierenga\Project\Timesheets\Domain\Model\User\User;
 use TijmenWierenga\Project\Timesheets\Domain\Model\User\UserId;
 use TijmenWierenga\Project\Timesheets\Domain\Model\User\UserRepository;
+use TijmenWierenga\Project\Timesheets\Domain\Projection\Projector;
 
 /**
  * @author Tijmen Wierenga <t.wierenga@live.nl>
@@ -16,14 +18,20 @@ class EventSourcedUserRepository implements UserRepository
      * @var EventStore
      */
     private $eventStore;
+    /**
+     * @var Projector
+     */
+    private $projector;
 
     /**
      * EventSourcedUserRepository constructor.
      * @param EventStore $eventStore
+     * @param Projector $projector
      */
-    public function __construct(EventStore $eventStore)
+    public function __construct(EventStore $eventStore, Projector $projector)
     {
         $this->eventStore = $eventStore;
+        $this->projector = $projector;
     }
 
     /**
@@ -45,6 +53,11 @@ class EventSourcedUserRepository implements UserRepository
      */
     public function save(User $user): void
     {
-        // TODO: Implement save() method.
+        $events = new EventStream($user->getUserId(), $user->recordedEvents());
+
+        $this->eventStore->append($events);
+        $user->clearEvents();
+
+        $this->projector->project($events);
     }
 }
