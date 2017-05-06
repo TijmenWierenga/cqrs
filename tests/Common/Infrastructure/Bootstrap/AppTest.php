3f9692ca742230db0dee790ca84e33c4a59aa26b
Tijmen Wierenga <t.wierenga@live.nl>
2017-05-06 22:03:31 +0200

    Added test to confirm Projector compiler pass is working

--- tests/Common/Infrastructure/Bootstrap/AppTest.php
+++ tests/Common/Infrastructure/Bootstrap/AppTest.php
@@ -1,9 +1,12 @@
 <?php
-namespace TijmenWierenga\Project\Tests\Shared\Infrastructure\Bootstrap;
+namespace TijmenWierenga\Project\Tests\Common\Infrastructure\Bootstrap;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionProperty;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use TijmenWierenga\Project\Common\Domain\Projection\Projector;
 use TijmenWierenga\Project\Common\Infrastructure\Bootstrap\App;
+use TijmenWierenga\Project\Common\Infrastructure\Bootstrap\CompilerPass\ProjectorCompilerPass;
 use TijmenWierenga\Project\Common\Infrastructure\Bootstrap\EnvironmentException;
 
 /**
@@ -28,10 +31,32 @@ class AppTest extends TestCase
      * @test
      * @depends it_runs_the_application
      * @param App $app
+     * @return App
      */
-    public function an_app_has_an_environment(App $app)
+    public function an_app_has_an_environment(App $app): App
     {
     	$this->assertEquals('test', $app::environment());
+
+    	return $app;
+    }
+
+    /**
+     * @test
+     * @depends an_app_has_an_environment
+     * @param App $app
+     */
+    public function it_registers_the_projector_with_listeners(App $app)
+    {
+    	$projector = $app::container()->get(ProjectorCompilerPass::SERVICE_NAME);
+
+    	$this->assertInstanceOf(Projector::class, $projector);
+
+    	$property = new ReflectionProperty($projector, 'projections');
+    	$property->setAccessible(true);
+    	$projections = $property->getValue($projector);
+    	$property->setAccessible(false);
+
+    	$this->assertNotEmpty($projections);
     }
 
     /**
