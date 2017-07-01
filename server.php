d72b155e66e5bb12f40d19449a6e44fbf5143401
Tijmen Wierenga <t.wierenga@live.nl>
2017-07-01 15:38:55 +0200

    Changed argument order

--- server.php
+++ server.php
@@ -11,9 +11,12 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new App();
 $app->run(getenv('APP_ENV'));
 
-$requestHandler = new ContainerAwareRequestHandler(App::container());
 $connection = new Connection();
+$requestHandler = new ContainerAwareRequestHandler(App::container());
+
 $server = new ReactPhpServer($connection, $requestHandler);
+
 echo "Server is running on {$connection->getIpAddress()}:{$connection->getPort()} with environment: "
     . App::environment() . PHP_EOL;
+
 $server->run();
